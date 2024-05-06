4f4cc39f453d4ef0fad37e3b8fc6dc5f247d890d
Miguel Sosa <85181687+msosav@users.noreply.github.com.>
2024-05-05 21:45:46 -0500

    Update scripts

--- Scripts/delete.sh
+++ Scripts/delete.sh
@@ -4,4 +4,5 @@ cd ../Configuration/
 
 kubectl delete -f efs.yml
 kubectl delete -f wordpress.yml
+kubectl delete -f ssl/ssl-issuer.yml
 kubectl delete -f nginx.yml
--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -4,4 +4,5 @@ cd ../Configuration/
 
 kubectl apply -f efs.yml
 kubectl apply -f wordpress.yml
+kubectl apply -f ssl/ssl-issuer.yml
 kubectl apply -f nginx.yml
